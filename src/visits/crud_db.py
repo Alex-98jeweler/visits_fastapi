822300b062682e3a393debf99c86d69f1161f9bc
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-07-20 12:33:41 +0700

    edit crud operations

--- src/visits/crud_db.py
+++ src/visits/crud_db.py
@@ -1,16 +1,32 @@
 from datetime import datetime
-from sqlalchemy import select, insert, and_
+from typing import List
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .models import visit
+from .schemas import HttpUrl
 
 
-async def get_domains(session: AsyncSession, from_: datetime = None, to: datetime = None):
+async def get_domains(
+    session: AsyncSession,
+    from_: datetime = None,
+    to: datetime = None
+):
 
     query = select(visit)
     if from_: query = query.where(visit.c.visited_at >= from_)
     if to: query = query.where(visit.c.visited_at <= to)
-    print(query)
-    result = await session.execute(query)
+    result = await session.execute(query.distinct())
     return result.fetchall()
+
+
+async def create_visit(
+    links: List[HttpUrl],
+    session: AsyncSession
+):
+    for link in links:
+        query = insert(visit).values(link=link.__str__())
+        await session.execute(query)
+    await session.commit()
+
     
