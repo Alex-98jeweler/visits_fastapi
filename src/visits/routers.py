238c3b478760dd1f15bda0c974c4682d5ae56569
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-07-20 11:49:15 +0700

    edited router module

--- src/visits/routers.py
+++ src/visits/routers.py
@@ -1,9 +1,12 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from .utils import from_timestamp_to_datetime
 from .schemas import VisitedLinks
-
+from . import crud_db
 from src.database import get_async_session
+
 
 
 router = APIRouter()
@@ -15,3 +18,15 @@ async def visited_links(
     session: AsyncSession = Depends(get_async_session)
 ):
     return {"Hi hi": "ha ha"}
+
+
+@router.get('/visited_domains',)
+async def visited_domains(
+    from_: int = None,
+    to: int = None,
+    session: AsyncSession = Depends(get_async_session)
+):
+    from_dt = from_timestamp_to_datetime(from_)
+    to_dt = from_timestamp_to_datetime(to)
+    await crud_db.get_domains(session, from_dt, to_dt)
+    return {}
