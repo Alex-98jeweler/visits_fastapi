38858cf7a00eccb0caf0b337e5bf9a16638de445
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-07-20 10:51:25 +0700

    add post route

--- src/visits/routers.py
+++ src/visits/routers.py
@@ -1,4 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from .schemas import VisitedLinks
+
+from src.database import get_async_session
 
 
 router = APIRouter()
+
+
+@router.post('/visited_links')
+async def visited_links(
+    visited_links: VisitedLinks,
+    session: AsyncSession = Depends(get_async_session)
+):
+    print(visited_links)
+    return {"Hi hi": "ha ha"}
