8c7cc0623f1d12be0ee74e03f172e184c8e45f08
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-07-20 16:03:22 +0700

    add new schemas and create utils for building response

--- src/visits/schemas.py
+++ src/visits/schemas.py
@@ -1,6 +1,18 @@
-from typing import List
+from typing import Any, List
 from pydantic import BaseModel, HttpUrl
+from enum import Enum
 
 
 class VisitedLinks(BaseModel):
     links: List[HttpUrl]
+
+
+class Statuses(str, Enum):
+    success = 'success'
+    error = 'error'
+
+
+class CommonResponses(BaseModel):
+    status: Statuses
+    message: str
+    data: Any
--- src/visits/utils.py
+++ src/visits/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-
+from typing import Any
+from .schemas import CommonResponses, Statuses
 
 def from_timestamp_to_datetime(
     timestamp: int
@@ -7,3 +8,19 @@ def from_timestamp_to_datetime(
     if not timestamp:
         return None
     return datetime.fromtimestamp(timestamp)
+
+def build_response_body(
+    is_success: bool,
+    message: str = '',
+    data: Any = []
+) -> CommonResponses:
+    response_dict = {}
+    if is_success:
+        response_dict['status'] = Statuses.success
+    else:
+        response_dict['status'] = Statuses.error
+    response_dict['message'] = message
+    response_dict['data'] = data
+    response = CommonResponses.parse_obj(response_dict)
+    return response
+
