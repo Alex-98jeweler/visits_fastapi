50856e5770730a636aea04d34468a7d27bbcc3f9
Alexandr Zubakin <zubakin.alexandr98@gmail.com>
2023-07-22 17:10:17 +0700

    add tests cases

--- tests/visits/test_api.py
+++ tests/visits/test_api.py
@@ -1,10 +1,9 @@
+from datetime import datetime
 import pytest
 from fastapi.testclient import TestClient
 from httpx import AsyncClient
 
 from src.main import app
-
-
 
 
 async def test_success_resposne(ac: AsyncClient):
@@ -14,6 +13,7 @@ async def test_success_resposne(ac: AsyncClient):
     response = await ac.post('/visited_links', json=links)
     assert response.status_code == 200
     assert response.json() == {"status": "success", "message": "success", "data": []}
+
 
 async def test_validation_error_response(ac: AsyncClient):
     links = {
@@ -28,3 +28,26 @@ async def test_get_domains(ac: AsyncClient):
     json = response.json()
     assert response.status_code == 200
     assert json['status'] == 'success'
+
+
+async def test_get_with_params(ac: AsyncClient):
+    response = await ac.get(
+        '/visited_domains',
+        params={
+            'from_': datetime.now().timestamp() - 3600,
+            'to': datetime.now().timestamp()
+        }
+    )
+    json = response.json()
+    assert json['status'] == 'success'
+
+
+async def test_get_with_wrong_params(ac: AsyncClient):
+    response = await ac.get(
+        '/visited_domains',
+        params={
+            'from_': 'wrong1',
+            'to': 'wrong2'
+        }
+    )
+    assert response.status_code == 422
